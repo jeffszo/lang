e8947bf3f6309fdab6e06f2553060dcccd36fcae
jeffszo <jefersonferreira27@outlook.com>
2023-10-04 17:31:41 -0300

    lp done

---
+++ script.js
@@ -0,0 +1,41 @@
+const btn = document.getElementById('my-button')
+const result = document.getElementById('paragraph')
+
+const linguagues = [
+    'Javascript', 
+    'PHP', 
+    'Python', 
+    'Java', 
+    'C', 
+    'C++', 
+    'Delphi', 
+    'C#', 
+    'Cobol',
+    'Ruby',
+    'Dart', 
+    'Typescript', 
+    'GO', 
+    'Perl', 
+    'Ruby', 
+    'Kotlin', 
+    'MATLAB', 
+    'Groovy', 
+    'Haskell', 
+    'Scala',
+    'Brainfuck' , 
+    'Whitespace' , 
+    'Malbolge' , 
+    'INTERCAL' , 
+    'Befunge' , 
+    'Piet', 
+    'Ook!' , 
+    'ArnoldC' ,
+    'Lua'
+]
+
+
+btn.addEventListener('click', function(){
+    const randomIndex = Math.floor(Math.random() * linguagues.length)
+    result.innerText  = linguagues[randomIndex];
+
+})
